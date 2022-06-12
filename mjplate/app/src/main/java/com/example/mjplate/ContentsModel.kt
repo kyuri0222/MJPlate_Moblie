437b8be848e8f6aafe034739728438790c313819
Kyuri Kim <kuri222@naver.com>
2022-06-12 16:09:03 +0900

    initial commit

--- mjplate/app/src/main/java/com/example/mjplate/ContentsModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mjplate
-
-data class ContentsModel (
-
-    val url : String = "",
-    val ImageUrl : String = "",
-    val titleText : String = ""
-
-    )
--- mjplate/app/src/main/java/com/example/mjplate/MainActivity.kt
+++ mjplate/app/src/main/java/com/example/mjplate/MainActivity.kt
@@ -2,46 +2,10 @@ package com.example.mjplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = mutableListOf<ContentsModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        items.add(
-            ContentsModel(
-                "https://www.mangoplate.com/restaurants/XRoMziImmYCC",
-                "https://mp-seoul-image-production-s3.mangoplate.com/331247/60039_1596540913676_34054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "뉴욕택시디저트"
-            )
-        )
-
-        items.add(
-            ContentsModel(
-                "https://www.mangoplate.com/restaurants/XRoMziImmYCC",
-                "https://mp-seoul-image-production-s3.mangoplate.com/331247/60039_1596540913676_34054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "뉴욕택시디저트"
-            )
-        )
-
-        items.add(
-            ContentsModel(
-                "https://www.mangoplate.com/restaurants/XRoMziImmYCC",
-                "https://mp-seoul-image-production-s3.mangoplate.com/331247/60039_1596540913676_34054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "뉴욕택시디저트"
-            )
-        )
-
-        val recyclerview = findViewById<RecyclerView>(R.id.rv)
-        val rvAdapter = RVAdapter(items)
-        recyclerview.adapter = rvAdapter
-
-        recyclerview.layoutManager = GridLayoutManager(this,2)
     }
 }
